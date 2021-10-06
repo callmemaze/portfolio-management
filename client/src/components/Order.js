b6a919727b5ff8c5ec47ac20f878807b701287b4
Dipesh Shrestha <dipeshshtha4@gmail.com>
2021-10-06 23:24:24 +0545

    Maze Commit

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -13,6 +13,13 @@ import Button from "@mui/material/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchName } from "./actions/name";
 import { createOrder } from "./actions/order";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
 
 const AntSwitch = styled(Switch)(({ theme }) => ({
   width: 28,
@@ -57,8 +64,51 @@ const AntSwitch = styled(Switch)(({ theme }) => ({
     boxSizing: "border-box",
   },
 }));
+
+const columns = [
+  { id: "number", label: "S.N", minWidth: 170 },
+  { id: "name", label: "Stock name", minWidth: 100 },
+  {
+    id: "type",
+    label: "Transaction Type",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "quantity",
+    label: "Quantity",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "amount",
+    label: "Amount",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+  {
+    id: "date",
+    label: "Transaction Date",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+];
+
+function createData(number, name, type, quantity, amount, date) {
+  return { number, name, type, quantity, amount, date };
+}
+
 const Order = () => {
   const [checked, setChecked] = useState(false);
+  const [error, setError] = useState();
+  const [num, setNum] = useState(0);
+  const [rows, setRows] = useState([
+    createData(1, "Standard Chartered Bank", "buy", 100, 500, "01/01/2021"),
+  ]);
   const initialState = {
     name: "",
     type: checked ? "Buy" : "Sell",
@@ -68,11 +118,33 @@ const Order = () => {
   };
   const [data, setData] = useState(initialState);
   const dispatch = useDispatch();
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(createOrder(data));
-    console.log(data);
+    if (
+      data.name === "" ||
+      data.type === "" ||
+      data.price === "" ||
+      data.date === "" ||
+      data.quantity === ""
+    ) {
+      setError(true);
+    } else {
+      dispatch(createOrder(data));
+      setNum(num + 1);
+      setRows([
+        ...rows,
+        createData(
+          num,
+          data.name,
+          data.type,
+          data.quantity,
+          data.price,
+          data.date
+        ),
+      ]);
+      clear();
+      setError(false);
+    }
   };
   useEffect(() => {
     dispatch(fetchName());
@@ -92,6 +164,21 @@ const Order = () => {
   const handleSwitchChange = (event) => {
     setChecked(event.target.checked);
     setData({ ...data, type: type });
+  };
+
+  const clear = () => {
+    setData(initialState);
+  };
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
   };
   return (
     <Box
@@ -115,7 +202,7 @@ const Order = () => {
             lg={12}
             sx={{ display: "flex", justifyContent: "center" }}
           >
-            <form onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate onSubmit={handleSubmit}>
               <Paper
                 sx={{
                   height: 420,
@@ -144,11 +231,10 @@ const Order = () => {
                     sx={{ height: 17, alignSelf: "end" }}
                   >
                     <Typography>Sell</Typography>
-                    <AntSwitch
+                    <Switch
                       name="checked"
                       checked={checked}
                       onChange={handleSwitchChange}
-                      inputProps={{ "aria-label": "ant design" }}
                     />
                     <Typography>Buy</Typography>
                   </Stack>
@@ -161,11 +247,12 @@ const Order = () => {
                     size="small"
                     id="combo-box-demo"
                     options={stockArray}
+                    value={data.name}
                     sx={{ width: 300 }}
                     filterSelectedOptions
                     onChange={(e, value) => setData({ ...data, name: value })}
                     renderInput={(params) => (
-                      <TextField {...params} label="Stock Name" />
+                      <TextField {...params} label="Stock Name" error={error} />
                     )}
                   />
                   <TextField
@@ -173,7 +260,9 @@ const Order = () => {
                     variant="outlined"
                     label="Quantity"
                     size="small"
+                    error={error}
                     sx={{ width: 300 }}
+                    value={data.quantity}
                     onChange={(e) =>
                       setData({ ...data, quantity: e.target.value })
                     }
@@ -183,7 +272,9 @@ const Order = () => {
                     variant="outlined"
                     label="Price"
                     size="small"
+                    error={error}
                     sx={{ width: 300 }}
+                    value={data.price}
                     onChange={(e) =>
                       setData({ ...data, price: e.target.value })
                     }
@@ -193,7 +284,9 @@ const Order = () => {
                     variant="outlined"
                     label="Date"
                     size="small"
+                    error={error}
                     sx={{ width: 300 }}
+                    value={data.date}
                     onChange={(e) => setData({ ...data, date: e.target.value })}
                   />
                   <Button
@@ -210,9 +303,76 @@ const Order = () => {
             </form>
           </Grid>
           <Grid item xs={12}>
-            <Paper
-              sx={{ p: 2, display: "flex", flexDirection: "column" }}
-            ></Paper>
+            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+              <Typography
+                component="h1"
+                variant="h5"
+                color="inherit"
+                sx={{ alignSelf: "center" }}
+              >
+                Order Book
+              </Typography>
+              <Paper sx={{ width: "100%", overflow: "hidden" }}>
+                <TableContainer sx={{ maxHeight: 440 }}>
+                  <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                      <TableRow>
+                        {columns.map((column) => (
+                          <TableCell
+                            key={column.id}
+                            align={column.align}
+                            style={{ minWidth: column.minWidth }}
+                          >
+                            {column.label}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows
+
+                        .slice(
+                          page * rowsPerPage,
+                          page * rowsPerPage + rowsPerPage
+                        )
+                        .map((row) => {
+                          return (
+                            <TableRow
+                              hover
+                              role="checkbox"
+                              tabIndex={-1}
+                              key={row.code}
+                            >
+                              {columns.map((column) => {
+                                const value = row[column.id];
+                                return (
+                                  <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                  >
+                                    {column.format && typeof value === "number"
+                                      ? column.format(value)
+                                      : value}
+                                  </TableCell>
+                                );
+                              })}
+                            </TableRow>
+                          );
+                        })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                <TablePagination
+                  rowsPerPageOptions={[10, 25, 100]}
+                  component="div"
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </Paper>
+            </Paper>
           </Grid>
         </Grid>
       </Container>
